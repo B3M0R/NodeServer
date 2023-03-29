08af30e43cae1f286169eda94fba86f9e0724542
Ben Moreno <Bemor8789@gmail.com>
2023-03-28 19:16:18 -0700

    2nd update nodejs

--- index.js
+++ index.js
@@ -2,22 +2,19 @@ import http from "http";
 import fetch from "node-fetch";
 import { createDeflateRaw } from "zlib";
 
-const server = http.createServer((req, res) => {
+const server = http
+  .createServer((req, res) => {
     const url = req.url;
-    let tableData =
-      "<table border='1'><tr><th>name</th><th>height</th><th>birth_year</th><th>gender</th><th>url</th></tr>"
-    if (url === "/") {
-      res.write("<h1>Home Page</h1>");
-      res.write(
-        '<img src="https://dummyimage.com/600x400/000000/8c00ff.jpg&text=Welcome!">'
-      );
-      res.end();
-    }
+    let tableData ="<table border='1'><tr><th>name</th><th>height</th><th>birth_year</th><th>gender</th><th>url</th></tr>"
 
-    if (url === "/list") {
-      fetch("https://swapi.dev/api/people")
-        .then((res) => res.json())
-        .then((data) => {
+    if (url === '/') {
+      res.write("<h1>Home Page</h1>");
+      res.write("<img src='https://dummyimage.com/600x400/000000/8c00ff.jpg&text=Welcome!'>")
+      res.end();
+    }else if (url === '/list') {
+      fetch('https://swapi.dev/api/people')
+        .then(res => res.json())
+        .then(data => {
           createData(data);
           res.write("<h1>Welcome To My Homepage</h1>");
           res.write(tableData);
@@ -29,10 +26,9 @@ const server = http.createServer((req, res) => {
     }
 
     function createData(data) {
-      data.results.forEach((element) => {
-        tableData += `<tr><td>${element.name}</td><td>${element.height}</td><td>${element.birth_year}</td><td>${element.gender}</td><td>${element.url}</td></tr>`;
+      data['results'].forEach(element => {
+        tableData+= `<tr><td>${element.name}</td><td>${element.height}</td><td>${element.birth_year}</td><td>${element.gender}</td><td>${element.url}</td></tr>`
       });
-      tableData += `</table>`;
+      tableData+=`</table>`
     }
-  })
-  .listen(6923, console.log(`Server is listening on port: 6923`));
+  }).listen(6923, console.log(`Server is listening on port: 6923`));
