0d2a17e35894513f2f9ae19d9084c7cf32371efc
Ben Moreno <Bemor8789@gmail.com>
2023-03-28 19:02:33 -0700

    update nodejs

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 import http from "http";
-import fetch from "node-fetch"
-import { createDeflateRaw } from "zlib"
+import fetch from "node-fetch";
+import { createDeflateRaw } from "zlib";
 
-  const server = http.createServer((req, res) =>{ 
-    const url = req.url
+const server = http.createServer((req, res) => {
+    const url = req.url;
     let tableData =
-    "<table border='1'><tr><th>name</th><th>height</th><th>birth_year</th><th>gender</th><th>url</th></tr>";
+      "<table border='1'><tr><th>name</th><th>height</th><th>birth_year</th><th>gender</th><th>url</th></tr>"
     if (url === "/") {
-      res.write("<h1>Home Page</h1>")
-      res.write('<img src="https://dummyimage.com/600x400/000000/8c00ff.jpg&text=Welcome!">')
+      res.write("<h1>Home Page</h1>");
+      res.write(
+        '<img src="https://dummyimage.com/600x400/000000/8c00ff.jpg&text=Welcome!">'
+      );
       res.end();
     }
 
-    if (url === '/list') {   
-      fetch('https://swapi.dev/api/people')
-        .then(res => res.json())
-        .then(data => {
-          createData(data)
-          res.write("<h1>Welcome To My Homepage</h1>")
+    if (url === "/list") {
+      fetch("https://swapi.dev/api/people")
+        .then((res) => res.json())
+        .then((data) => {
+          createData(data);
+          res.write("<h1>Welcome To My Homepage</h1>");
           res.write(tableData);
-          res.end()
-        })
-      } 
-    else {
-          res.write("<h1>Page Not Found</h1>")
           res.end();
-        }
-
-
-    function createData(data) {
-      data.results.forEach(element => {
-        tableData += `<tr><td>${element.name}</td><td>${element.height}</td><td>${element.birth_year}</td><td>${element.gender}</td><td>${element.url}</td>`
-      });
-      tableData += `</table>`
+        });
+    } else {
+      res.write("<h1>Page Not Found</h1>");
+      res.end();
     }
 
-  }).listen(6923, console.log("server is listening on port" + 6923));
+    function createData(data) {
+      data.results.forEach((element) => {
+        tableData += `<tr><td>${element.name}</td><td>${element.height}</td><td>${element.birth_year}</td><td>${element.gender}</td><td>${element.url}</td></tr>`;
+      });
+      tableData += `</table>`;
+    }
+  })
+  .listen(6923, console.log(`Server is listening on port: 6923`));
